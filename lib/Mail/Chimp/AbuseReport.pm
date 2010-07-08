d16f10ea7dab1dba90d16070921c4f76baf4a4c9
Gautam Dey <gautam.dey77@gmail.com>
2010-07-07 18:05:17 -0700

    Removed dependence on ::DateTimeX and updated the type for campaign id.

--- lib/Mail/Chimp/AbuseReport.pm
+++ lib/Mail/Chimp/AbuseReport.pm
@@ -2,12 +2,14 @@ package Mail::Chimp::AbuseReport;
 use strict;
 use warnings;
 use Moose;
-use MooseX::Types::DateTimeX qw(DateTime);
+# I'm too lazy to port over ::DateTimeX to etch. So, for now it is just a string.
+#  Gautam.
+#use MooseX::Types::DateTimeX qw(DateTime);
 our $VERSION = '0.2.1';
 
-has 'date'        => (is => 'ro', isa => DateTime, coerce => 1);
+has 'date'        => (is => 'ro', isa => 'Str');
 has 'email'       => (is => 'ro', isa => 'Str');
-has 'campaign_id' => (is => 'ro', isa => 'Int');
+has 'campaign_id' => (is => 'ro', isa => 'Str'); # Campaign id is a string according to API docs for 1.2
 has 'type'        => (is => 'ro', isa => 'Str');
 
 1;
